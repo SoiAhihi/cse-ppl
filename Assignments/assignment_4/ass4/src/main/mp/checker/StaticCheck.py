599d023fa7d4bdcad2cb34af7fa220ca946a7121
SoiAhihi <62455700+SoiAhihi@users.noreply.github.com>
2020-12-09 13:48:24 +0700

    second submit

--- Assignments/assignment_4/ass4/src/main/mp/checker/StaticCheck.py
+++ Assignments/assignment_4/ass4/src/main/mp/checker/StaticCheck.py
@@ -2,56 +2,190 @@
 """
  * @author nhphung
 """
-from AST import *
+from abc import ABC, abstractmethod, ABCMeta
+from dataclasses import dataclass
+from typing import List, Tuple
+from AST import * 
 from Visitor import *
-from Utils import Utils
 from StaticError import *
+from functools import *
 
+class Type(ABC):
+    __metaclass__ = ABCMeta
+    pass
+class Prim(Type):
+    __metaclass__ = ABCMeta
+    pass
+class IntType(Prim):
+    pass
+class FloatType(Prim):
+    pass
+class StringType(Prim):
+    pass
+class BoolType(Prim):
+    pass
+class VoidType(Type):
+    pass
+class Unknown(Type):
+    pass
+
+@dataclass
+class ArrayType(Type):
+    dimen:List[int]
+    eletype: Type
+
+@dataclass
 class MType:
-    def __init__(self, partype, rettype):
-        self.partype = partype
-        self.rettype = rettype
+    intype:List[Type]
+    restype:Type
 
-    def __str__(self):
-        return 'MType([' + ','.join([str(i) for i in self.partype]) + '],' + str(self.rettype) + ')'
-
+@dataclass
 class Symbol:
-    def __init__(self, name, mtype, value=None):
-        self.name = name
-        self.mtype = mtype
-        self.value = value
+    name: str
+    mtype:Type
 
-    def __str__(self):
-        return 'Symbol(' + self.name + ',' + str(self.mtype) + ',' + str(self.value) + ')'
-
-class StaticChecker(BaseVisitor, Utils):
-
-    global_envi = [
-        Symbol("getInt", MType([], IntType())),
-        Symbol("putIntLn", MType([IntType()], VoidType()))]
-
-    def __init__(self, ast):
+class StaticChecker(BaseVisitor):
+    def __init__(self,ast):
         self.ast = ast
-
+        self.global_envi = [
+Symbol("int_of_float",MType([FloatType()],IntType())),
+Symbol("float_of_int",MType([IntType()],FloatType())),
+Symbol("int_of_string",MType([StringType()],IntType())),
+Symbol("string_of_int",MType([IntType()],StringType())),
+Symbol("float_of_string",MType([StringType()],FloatType())),
+Symbol("string_of_float",MType([FloatType()],StringType())),
+Symbol("bool_of_string",MType([StringType()],BoolType())),
+Symbol("string_of_bool",MType([BoolType()],StringType())),
+Symbol("read",MType([],StringType())),
+Symbol("printLn",MType([],VoidType())),
+Symbol("printStr",MType([StringType()],VoidType())),
+Symbol("printStrLn",MType([StringType()],VoidType()))]                           
+   
     def check(self):
-        return self.visit(self.ast, StaticChecker.global_envi)
+        return self.visit(self.ast,self.global_envi)
 
-    def visitProgram(self, ast, c):
-        return [self.visit(x, c) for x in ast.decl]
 
-    def visitFuncDecl(self, ast, c):
-        return list(map(lambda x: self.visit(x, (c, True)), ast.body))
 
-    def visitCallStmt(self, ast, c):
-        at = [self.visit(x, (c[0], False)) for x in ast.param]
-
-        res = self.lookup(ast.method.name, c[0], lambda x: x.name)
-        if res is None or not type(res.mtype) is MType or not type(res.mtype.rettype) is VoidType:
-            raise Undeclared(Procedure(), ast.method.name)
-        elif len(res.mtype.partype) != len(at):
-            raise TypeMismatchInStatement(ast)
+    def get(self,name,c):
+        local =[i for i in c[1]] 
+        unlocal = [i for i in c[0]]
+        for i in local:
+            if i.name == name:
+                return i.mtype
         else:
-            return res.mtype.rettype
+            for x in unlocal:
+                if x.name == name:
+                    return x.mtype
+            else:
+                return -1
+    def update(self,name,typ,c):
+        pass
 
-    def visitIntLiteral(self, ast, c):
+
+    def visitProgram(self,ast, c):
+        Global =[self.global_envi,[]]
+        reduce(lambda x,y: self.visit(y,x),ast.decl,Global)
+    
+
+    def visitVarDecl(self, ast, param):
+        name =ast.variable.name
+        if isinstance(param,list):
+            if self.get(name,param) != -1:
+                raise Redeclared(Variable(),name)
+            typ = Unknown()
+            if ast.varInit != None:
+                typ = self.visit(ast.varInit,param)
+            if isinstance(typ,ArrayType):
+                typ.dimen =  ast.varDimen
+            param[1].extend([Symbol(name,typ)])    
+            return param
+        else:
+            if name in [i.variable.name for i in param]:
+                raise Redeclared(Parameter(),name)
+            param += (Symbol(name,Unknown()))
+            return param
+    
+
+
+    def visitFuncDecl(self, ast, param):
+        if self.get(ast.name,param) != -1:
+                raise Redeclared(Function(),ast.name)
+        params = reduce(lambda x,y:self.visit(y,x),ast.param,())
+
+        param[1].extend([Symbol(ast.name,MType(list(params),VoidType()))])
+        unlocal = []
+        unlocal.extend(param[1])
+        for i in param[0]:
+            if i.name not in [x.name for x in unlocal]:
+                unlocal.extend([i])
+        else:
+            pass
+        local=[]
+        local.extend(list(params))
+
+        dec=[unlocal,local]
+        reduce(lambda x,y:self.visit(y,x),ast.body[0],dec)
+
+
+        return param
+
+    def visitBinaryOp(self, ast, param):
+        return None
+    
+    def visitUnaryOp(self, ast, param):
+        return None
+    
+    def visitCallExpr(self, ast, param):
+        return None
+    
+    def visitId(self, ast, param):
+        return None
+    
+    def visitArrayCell(self, ast, param):
+        return None
+    
+    def visitAssign(self, ast, param):
+        return None
+    
+    def visitIf(self, ast, param):
+        return None
+    
+    def visitFor(self, ast, param):
+        return None
+    
+    def visitContinue(self, ast, param):
+        return None
+    
+    def visitBreak(self, ast, param):
+        return None
+    
+    def visitReturn(self, ast, param):
+        return None
+    
+    def visitDowhile(self, ast, param):
+        return None
+
+    def visitWhile(self, ast, param):
+        return None
+
+    def visitCallStmt(self, ast, param):
+        return None
+
+    def visitIntLiteral(self, ast, param):
         return IntType()
+    
+    def visitFloatLiteral(self, ast, param):
+        return FloatType()
+    
+    def visitBooleanLiteral(self, ast, param):
+        return BoolType()
+    
+    def visitStringLiteral(self, ast, param):
+        return StringType()
+
+    def visitArrayLiteral(self, ast, param):
+        init = self.visit(ast.value[0],param)
+        for i in [self.visit(_,param) for _ in ast.value[1:]]:
+            if type(i) != type(init):
+                pass
+        return ArrayType([],init)
